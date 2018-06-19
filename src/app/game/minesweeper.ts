e47cc3f7623cbaea71f95639330870b1bedbafbd
Pablo Aravena <paravena@homeemergency.com>
2018-06-18 22:13:34 -0400

    most of the game logic is finished

--- src/app/game/minesweeper.ts
+++ src/app/game/minesweeper.ts
@@ -1,29 +1,120 @@
 import * as R from 'ramda';
-import {Tile} from './tile.model';
+import { Tile } from './tile.model';
 
 const NUMBER_OF_ROWS = 16;
 const NUMBER_OF_COLS = 16;
 const NUMBER_OF_MINES = 64;
 
+const newMineTile = () => {
+  return {isMine: true, visible: false} as Tile;
+};
+
+const newTile = () => {
+  return {isMine: false, visible: false, mineCounter: 0} as Tile;
+};
+
 const initTiles = (rows: number, cols: number, mines: number) => {
-  let tiles = R.times(() => new Tile(true), mines);
-  tiles = R.concat(R.times(() => new Tile(false), rows * cols - mines), tiles);
-  return shuffle(tiles);
+  let tiles = R.times(newMineTile, mines);
+  tiles = shuffle(R.concat(R.times(newTile, rows * cols - mines), tiles));
+  return tiles;
 };
 
 const shuffle = (tiles: Array<Tile>) => {
   return R.sort(() => Math.random() - 0.5, tiles);
 };
 
+const indexExists = R.both(R.lte(0), R.gte( NUMBER_OF_COLS * NUMBER_OF_ROWS - 1));
+const isAMine = R.curry((tiles: Array<Tile>, index: number) => tiles[index].isMine);
+const tileIsAMine = (tile: Tile) => tile.isMine;
+const tileIsNotAMine = R.compose(R.not, tileIsAMine);
+const isNotVisible = R.curry((tiles: Array<Tile>, index: number) => !tiles[index].visible);
+
+const indexExistAndHasNotBeenRevealed = R.curry((tiles: Array<Tile>, index: number) => {
+  return R.both(indexExists, isNotVisible(tiles))(index);
+});
+
+const getTileIfExistAndNotBeenRevealed = R.curry((tiles: Array<Tile>, index: number): Tile => {
+  const tile = R.ifElse(indexExistAndHasNotBeenRevealed(tiles), (idx: number) => tiles[idx], () => null)(index);
+  R.ifElse(notNil, () => tile.index = index, R.empty)(tile);
+  return tile;
+});
+
+const isEEdge = (index: number) => index % NUMBER_OF_COLS === NUMBER_OF_COLS - 1;
+const isWEdge = (index: number) => index % NUMBER_OF_COLS === 0;
+
+const n = (tiles: Array<Tile>, index: number) => {
+  return getTileIfExistAndNotBeenRevealed(tiles, index - NUMBER_OF_COLS);
+};
+
+const s = (tiles: Array<Tile>, index: number) => {
+  return getTileIfExistAndNotBeenRevealed(tiles, index + NUMBER_OF_COLS);
+};
+
+const e = (tiles: Array<Tile>, index: number) => {
+  return R.ifElse(isEEdge, () => null, () => getTileIfExistAndNotBeenRevealed(tiles, index + 1))(index);
+};
+
+const w = (tiles: Array<Tile>, index: number) => {
+  return R.ifElse(isWEdge, () => null, () => getTileIfExistAndNotBeenRevealed(tiles, index - 1))(index);
+};
+
+const se = (tiles: Array<Tile>, index: number) => {
+  return R.ifElse(isEEdge, () => null, () => getTileIfExistAndNotBeenRevealed(tiles, index + NUMBER_OF_COLS + 1))(index);
+};
+
+const sw = (tiles: Array<Tile>, index: number) => {
+  return R.ifElse(isWEdge, () => null, () => getTileIfExistAndNotBeenRevealed(tiles, index + NUMBER_OF_COLS - 1))(index);
+};
+
+const ne = (tiles: Array<Tile>, index: number) => {
+  return R.ifElse(isEEdge, () => null, () => getTileIfExistAndNotBeenRevealed(tiles, index - NUMBER_OF_COLS + 1))(index);
+};
+
+const nw = (tiles: Array<Tile>, index: number) => {
+  return R.ifElse(isWEdge, () => null, () => getTileIfExistAndNotBeenRevealed(tiles, index - NUMBER_OF_COLS - 1))(index);
+};
+
+const notNil = R.compose(R.not, R.isNil);
+
+const DIRECTIONS = [nw, n, ne, e, se, s, sw, w];
+
+const neighbors = R.curry((tiles: Array<Tile>, index: number): Array<Tile> => {
+  const getTileFromDir = (dir) => {
+    return dir(tiles, index);
+  };
+  return R.filter(notNil, R.map(getTileFromDir, DIRECTIONS));
+});
+
+const getMineCounter = (tiles: Array<Tile>, index: number) => {
+  return R.length(R.filter(tileIsAMine, neighbors(tiles, index)));
+};
+
+
+const revealAdjacentTiles = R.curry((tiles: Array<Tile>, index: number) => {
+  const mineCounter = getMineCounter(tiles, index);
+  tiles[index].mineCounter = mineCounter;
+  tiles[index].visible = true;
+  const processAdjacentTiles = () => {
+    return R.reduce((tilesGrid: Array<Tile>, tile: Tile) => {
+      return R.ifElse(isNotVisible(tilesGrid), revealAdjacentTiles(tilesGrid), () => tilesGrid)(tile.index);
+    }, tiles)(R.filter(tileIsNotAMine, neighbors(tiles, index)));
+  };
+  tiles = R.ifElse(R.equals(0), processAdjacentTiles, () => tiles)(mineCounter);
+  return tiles.slice();
+});
+
+const gameOver = R.curry((tiles: Array<Tile>, index: number) => {
+  console.log('game over');
+  return tiles;
+});
+
 export const initGame = () => {
   return initTiles(NUMBER_OF_ROWS, NUMBER_OF_COLS, NUMBER_OF_MINES);
 };
 
-export const revealTile = (tileIndex: number, tiles: Array<Tile>) => {
-  tiles[tileIndex].visible = true;
-  if (!tiles[tileIndex].isMine) {
-    tiles[tileIndex].mineCounter++;
-  }
+export const revealTile = (index: number, tiles: Array<Tile>) => {
+  tiles[index].visible = true;
+  tiles = R.ifElse(isAMine(tiles), gameOver(tiles), revealAdjacentTiles(tiles))(index);
   return tiles.slice();
 };
 
--- src/app/game/tile.model.ts
+++ src/app/game/tile.model.ts
@@ -1,35 +1,6 @@
-export class Tile {
-  private _visible: boolean;
-  private _isMine: boolean;
-  private _mineCounter: number;
-
-  constructor(isMine: boolean) {
-    this._isMine = isMine;
-    this._mineCounter = 0;
-    this._visible = false;
-  }
-
-  get mineCounter(): number {
-    return this._mineCounter;
-  }
-
-  set mineCounter(value: number) {
-    this._mineCounter = value;
-  }
-
-  get isMine(): boolean {
-    return this._isMine;
-  }
-
-  set isMine(value: boolean) {
-    this._isMine = value;
-  }
-
-  get visible(): boolean {
-    return this._visible;
-  }
-
-  set visible(value: boolean) {
-    this._visible = value;
-  }
+export interface Tile {
+  visible: boolean;
+  isMine: boolean;
+  mineCounter: number;
+  index: number;
 }
